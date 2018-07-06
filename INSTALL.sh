b532a27c86c7a533785b46dc38ff22b777467601
biskyt <github@gettheitguy.com>
2018-07-06 16:17:03 +0100

    fixed permission and typo in install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,6 +3,6 @@
 sudo cp backupscript.sh /usr/local/bin/
 sudo chmod a+rx /usr/local/bin/backupscript.sh
 sudo cp backup-docker-cronjob /etc/cron.d/
-sudo chmod a+rx /etc/cron.d/backup-docker-cronjob
+sudo chmod 644 /etc/cron.d/backup-docker-cronjob
 
-echo "Installed, will run at reboot - edit /etc/crontab.d/backup-docker-cronjob to change file locations"
+echo "Installed, will run at reboot - edit /etc/cron.d/backup-docker-cronjob to change file locations"
