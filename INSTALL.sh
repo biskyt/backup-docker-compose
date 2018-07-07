7b8967fa4aa4240e3bb3d69227bf849ffaa902ef
biskyt <github@gettheitguy.com>
2018-07-07 14:44:20 +0100

    fix expansion of HOME

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,7 +2,8 @@
 
 sudo cp backupscript.sh /usr/local/bin/
 sudo chmod a+rx /usr/local/bin/backupscript.sh
-sudo echo $(cat backup-docker-cronjob) /etc/cron.d/backup-docker-cronjob
+# Use sed to expand variable for home dir
+sed "s|\$HOME|$HOME|" backup-docker-cronjob | sudo tee /etc/cron.d/backup-docker-cronjob
 sudo chmod 644 /etc/cron.d/backup-docker-cronjob
 
-echo "Installed, will run at reboot - edit /etc/cron.d/backup-docker-cronjob to change file locations"
+echo "Installed, will run at above schedule edit /etc/cron.d/backup-docker-cronjob to change file locations"
