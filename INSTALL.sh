4f6a71ba0eabbe31d1594471fbe0132b7e763684
biskyt <github@gettheitguy.com>
2018-07-07 10:21:19 +0100

    fix echo

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,7 +2,7 @@
 
 sudo cp backupscript.sh /usr/local/bin/
 sudo chmod a+rx /usr/local/bin/backupscript.sh
-sudo echo $(backup-docker-cronjob) /etc/cron.d/backup-docker-cronjob
+sudo echo $(cat backup-docker-cronjob) /etc/cron.d/backup-docker-cronjob
 sudo chmod 644 /etc/cron.d/backup-docker-cronjob
 
 echo "Installed, will run at reboot - edit /etc/cron.d/backup-docker-cronjob to change file locations"
